236129599016a626ededef142c380e19332feecb
Tushar Saha <tsaha.cse@gmail.com>
2026-02-10 11:37:59 +0100

    Remove unused result module

--- lib/app/modules/result/controllers/result_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get/get.dart';
-
-import '/app/core/base/base_controller.dart';
-import '/app/modules/processing/models/processing_result.dart';
-
-class ResultController extends BaseController {
-  ProcessingResult? get result => Get.arguments as ProcessingResult?;
-}
